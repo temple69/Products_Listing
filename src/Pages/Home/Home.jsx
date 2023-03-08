27dab1ebbe5eefcbe19b8e3c445292562288c278
temple69 <utemple169@gmail.com>
2023-03-08 22:57:29 +0100

    component that renders the home

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,32 @@
+import React, {  useEffect, useState } from "react";
+import Cart from "../../Components/Cart/Cart";
+import Header from "../../Components/Header/Header";
+import ProductList from "../../Components/ProductList/ProductList";
+import useCartContext from "../../hooks/useCartContext";
+import useFetch from "../../hooks/useFetch";
+
+
+const Home = () => {
+  const cartData = useCartContext();
+  const { CartItems,removeFromCart,totalItems} = cartData;
+  const products = useFetch();
+  const [showCart,setShowCart]=useState(false)
+  const showCartHandler=()=>{
+    setShowCart(true)
+  }
+  
+  
+
+  return (
+    <>
+    <Header totalItems ={totalItems} openCartHandler={showCartHandler}/>
+    <main>
+      <ProductList products={products} />
+      {showCart?<Cart cartItems={CartItems}
+      onRemoveFromCart={removeFromCart}/>:""}
+    </main>
+    </>
+  );
+};
+
+export default Home;
