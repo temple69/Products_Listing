3ec4106e042c1dc551ad8b22f5560fdb78ab3ece
temple69 <utemple169@gmail.com>
2023-03-08 23:03:09 +0100

    react files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { CartContextProvider } from "./context/CartContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <CartContextProvider>
     <App />
+    </CartContextProvider>
   </React.StrictMode>
 );
 
